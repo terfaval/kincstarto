ce3323a96d2e076e7d963a64f2134fd2ca121c25
terfaval <mate.fater@gmail.com>
2026-03-24 09:14:38 +0100

    fixed view for iphone

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,26 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "header",
+            key: "accept",
+            value: ".*text/html.*",
+          },
+        ],
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
